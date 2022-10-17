a0a9fa72dd7b49a4775eacb839ff66951929c8ad
ilis137 <iliyasshahapure1@gmail.com>
2022-10-17 19:36:03 +0530

    craete tictactoe class

---
+++ javabasics/src/main/java/com/javaworkshop/TicTacToe.java
@@ -0,0 +1,29 @@
+package com.javaworkshop;
+
+
+/*
+ * As a Player would like to
+start fresh by creating a tic
+tac toe board - Create a TicTacToeGame class - Create method for every use case
+and call from main
+- Create a board of char[] of size 10
+and assign empty space to each
+element
+
+ */
+public class TicTacToe {
+  
+  char[] board=new char[10];
+
+  public TicTacToe(){
+
+    for(int i=1;i<board.length;i++){
+      board[i]=' ';
+    }
+  }
+  public static void main(String[] args) {
+    TicTacToe game=new TicTacToe();
+  }
+}
+
+
