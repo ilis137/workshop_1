99c57420e5b1a0cdb7fb6d8c0cd024796bc4ba9c
ilis137 <iliyasshahapure1@gmail.com>
2022-10-17 20:35:20 +0530

    add: user chooses sign to play the game

--- javabasics/src/main/java/com/javaworkshop/TicTacToe.java
+++ javabasics/src/main/java/com/javaworkshop/TicTacToe.java
@@ -1,18 +1,24 @@
 package com.javaworkshop;
 
+import java.util.Scanner;
+
 /*
- * As a Player would like to
-start fresh by creating a tic
-tac toe board - Create a TicTacToeGame class - Create method for every use case
-and call from main
-- Create a board of char[] of size 10
-and assign empty space to each
-element
+ Ability to allow the
+player to choose a
+letter X or O
+
+- Create a method to allow player to
+input X or O and call from main
+- Determine Player and Computer
+ Letter to play the game
 
  */
 public class TicTacToe {
 
   char[] board = new char[10];
+  char playerSign;
+  char computerSign;
+  Scanner sc=new Scanner(System.in);
 
   // initialize values of boaed
   public void createBoard() {
@@ -20,9 +26,19 @@ public class TicTacToe {
       board[i] = ' ';
     }
   }
+  
+
+  public void chooseSign(){
+    System.out.println("Choose X or O: ");
+    
+    playerSign=sc.next().toCharArray()[0];
+    computerSign=(playerSign=='X')?'O':'X';
+  }
+
 
   public static void main(String[] args) {
     TicTacToe game = new TicTacToe();
     game.createBoard();
+    game.chooseSign();
   }
 }
